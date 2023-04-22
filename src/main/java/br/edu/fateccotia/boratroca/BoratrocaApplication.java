ac90ca771f7ac4f0a04e0b9d22db48ace7136cf8
rickae <ricardo.santos.ferreira29@gmail.com>
2023-04-22 16:58:30 -0300

    Primeiro commit, base gerada com Spring initializr com Spring Web, Spring Data JPA e Mysql Driver

---
+++ src/main/java/br/edu/fateccotia/boratroca/BoratrocaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.fateccotia.boratroca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoratrocaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoratrocaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/fateccotia/boratroca/BoratrocaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.fateccotia.boratroca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoratrocaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
