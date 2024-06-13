b83f817e132fc5581234e521514c1ec16ed35345
Patricia Lais <patricialais@hotmail.com>
2024-06-13 15:36:48 -0300

    Alterar configuração global de CORS

--- src/main/java/br/edu/fateccotia/boratroca/security/CorsConfiguration.java
+++ src/main/java/br/edu/fateccotia/boratroca/security/CorsConfiguration.java
@@ -8,7 +8,9 @@ public class CorsConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200")
-            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
+            .allowedOrigins("http://localhost:4200", "https://boratrocar.net")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+            .allowedHeaders("*")
+            .allowCredentials(true);
     }
 }
