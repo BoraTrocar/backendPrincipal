9b116d0d72f3dbed1a2e9721c6d5b4e2171b79a6
PC <ricardo.santos.ferreira29@gmail.com>
2024-09-13 17:43:11 -0300

    Adicionado validação aos DTOs

--- src/main/java/br/edu/fateccotia/boratroca/dto/LivroDTO.java
+++ src/main/java/br/edu/fateccotia/boratroca/dto/LivroDTO.java
@@ -1,19 +1,25 @@
 package br.edu.fateccotia.boratroca.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class LivroDTO {
 	private int idLivro;
+	@NotNull
 	private String nomeLivro;
 	private String isbn;
 	private String descricao;
 	private MultipartFile imagemFile;
 	private byte[] imagemBlob;
+	@NotNull
 	private String condicao;
+	@NotNull
 	private String categoria;
+	@NotNull
 	private String autor;
+	@NotNull
 	private String usuario;
 
 	public LivroDTO() {
--- src/main/java/br/edu/fateccotia/boratroca/dto/UsuarioDTO.java
+++ src/main/java/br/edu/fateccotia/boratroca/dto/UsuarioDTO.java
@@ -1,12 +1,17 @@
 package br.edu.fateccotia.boratroca.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.stereotype.Component;
-
 
 @Component
 public class UsuarioDTO {
+	@NotNull
+	@Email
 	private String email;
-	
+
+	@Min(8)
 	private String senha;
 	
 	public String getEmail() {
