633501dcf7880c549fb4973fe8a429e3b0c13453
FlavioAlmeidaa <112271774+FlavioAlmeidaa@users.noreply.github.com>
2024-09-24 18:31:55 -0300

    configurando cors, Para a VM

--- src/main/java/br/edu/fateccotia/boratroca/security/CorsConfiguration.java
+++ src/main/java/br/edu/fateccotia/boratroca/security/CorsConfiguration.java
@@ -8,7 +8,7 @@ public class CorsConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200", "https://boratrocar.net")
+            .allowedOrigins("http://localhost:4200", "https://boratrocar.net","http://45.55.202.40:80")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
             .allowedHeaders("*")
             .allowCredentials(true);
