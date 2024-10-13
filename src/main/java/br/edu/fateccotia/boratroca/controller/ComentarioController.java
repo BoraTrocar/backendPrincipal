bb2938000b591d2c4d7a6db2a41ea7185447ecdb
Ricardo Santos Ferreira <ricardo.santos.ferreira29@gmail.com>
2024-10-13 10:55:26 -0300

    modificado o caminho da api

--- src/main/java/br/edu/fateccotia/boratroca/controller/ComentarioController.java
+++ src/main/java/br/edu/fateccotia/boratroca/controller/ComentarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/comentarios")
 public class ComentarioController {
     @Autowired
     ComentarioService comentarioService;
