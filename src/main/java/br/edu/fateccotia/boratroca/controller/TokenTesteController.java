2f8c30e171dac968c16d7b026fc4623ea5aa4a44
rickae <ricardo.santos.ferreira29@gmail.com>
2024-11-29 18:31:56 -0300

    Token teste

--- src/main/java/br/edu/fateccotia/boratroca/controller/TokenTesteController.java
+++ src/main/java/br/edu/fateccotia/boratroca/controller/TokenTesteController.java
@@ -3,14 +3,15 @@ package br.edu.fateccotia.boratroca.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/tokenteste")
 public class TokenTesteController {
-    @PostMapping
-    public ResponseEntity<?> mostrarToken(String token) {
+    @PostMapping("/teste")
+    public ResponseEntity<?> mostrarToken(@RequestBody String token) {
         return ResponseEntity.status(HttpStatus.OK).body(token);
     }
 }
