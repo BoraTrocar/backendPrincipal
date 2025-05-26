fb85ded6018eea079308de75fcdfe18056cf5664
Flávio Almeida <112271774+FlavioAlmeidaa@users.noreply.github.com>
2025-05-26 16:07:13 -0300

    alterando para azure http

--- src/main/java/br/edu/fateccotia/boratroca/security/CorsConfiguration.java
+++ src/main/java/br/edu/fateccotia/boratroca/security/CorsConfiguration.java
@@ -8,7 +8,7 @@ public class CorsConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:4200", "https://boratrocar.net")
+            .allowedOrigins("http://localhost:4200", "http://boratrocar.company")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
             .allowedHeaders("*")
             .allowCredentials(true);
